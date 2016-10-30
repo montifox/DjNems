4d02482b2580fa6af1a5a341afc031e8f9369b90
Mateusz Chojnacki <chojnackimateusz95@wp.pl>
2016-10-30 18:01:00 +0100

    Create nav and function
    - we have a first functionality that displays our songs

--- js/application.js
+++ js/application.js
@@ -1,2 +1,14 @@
-'use strict'; 
-angular.module( 'aplikacja' , [] );
+'use strict';
+
+var app = angular.module( 'app' , [] );
+
+app.controller( 'songs' , [ '$scope' , function( $scope ){
+	
+	$scope.songs = [
+		{ autor : 'Darude' , tytul : 'Sand storm' , url : '/darude/sandstorm', gatunek: 'trans tehno' },
+		{ autor : 'Sum-41' , tytul : 'Fake my own death' , url : '/sum41/fakemyowndeath', gatunek: 'punk' }
+	];
+
+	console.log(  );
+
+}]);
