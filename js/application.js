21cd087fd6aa4a4bba14230162249f1b325b4136
Mateusz Chojnacki <chojnackimateusz95@wp.pl>
2016-12-13 20:59:46 +0100

    Tworzenie  kontrolerów dla addmusic, music, orders, users
    *Stworzenie formularzu na podstornie partials/addusers.html
    *Poprawa błędu w partials/addmusic.html
    *Tworzenie podstrony partials/addmusic
    *Stworzenie kontrolera do dodawania użytkownika
    *Stworzenie $routeProvider dla addusers

--- js/application.js
+++ js/application.js
@@ -1,19 +1,45 @@
 'use strict';
 
-var app = angular.module( 'app' , ['ngRoute', 'controllers'] );
+var app = angular.module( 'app' , ['ngRoute', 'controllersAdmin','controllersNavigation','controllersSite'] );
 
 app.config([ '$routeProvider', function($routeProvider){
     
-    $routeProvider.when('/music',{
+    //--------------_Admin__Music_--------------
+    $routeProvider.when('/admin/music',{
         controller : 'songs',
-        templateUrl : 'partials/music.html'
+        templateUrl : 'partials/admin/music.html'
     });
     
-        $routeProvider.when('/music/addmusic',{
+    $routeProvider.when('/music/admin/addmusic',{
         controller : 'addmusic',
-        templateUrl : 'partials/addmusic.html'
+        templateUrl : 'partials/admin/addmusic.html'
     });
     
+    //---------------_Admin_User_----------------
+    $routeProvider.when('/admin/users',{
+        controller : 'users',
+        templateUrl : 'partials/admin/users.html'
+    });
+    
+
+    $routeProvider.when('/admin/users/adduser',{
+        controller : 'adduser',
+        templateUrl : 'partials/admin/adduser.html'
+    });
+    //---------------_Admin_Orders_---------------
+    $routeProvider.when('/admin/orders',{
+        controller : 'orders',
+        templateUrl : 'partials/admin/orders.html'
+    });
+    
+    //--------------_Site__Music_--------------
+    $routeProvider.when('/music',{
+        controller : 'showSongs',
+        templateUrl : 'partials/site/music.html'
+    });
+
+        
+    //----------_Others_-----------------
     $routeProvider.otherwise({
         redirectTo: '/home'
     });
---
+++ js/controllers/controllers-navigation.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var controllersNavigation = angular.module( 'controllersNavigation' , ['ngRoute'] );
+
+controllersNavigation.controller( 'navigation' , [ '$scope' , '$location', function( $scope, $location ){
+	
+    $scope.navigation =function(){
+     if(/^\/admin/.test($location.path()))
+         return 'partials/admin/navigation.html';
+     else     
+         return 'partials/site/navigation.html';
+    }
+
+}]);
+
+
